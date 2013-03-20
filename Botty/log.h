a1e0aa788a2ca55b78c321bdace1bd343773f73e
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-03-20 14:43:09 +0100

    implemented logging

--- Botty/log.h
+++ Botty/log.h
@@ -4,12 +4,20 @@
 
 namespace botty{
 	namespace log {
+		void print(std::string msg) {
+			std::cout << msg << std::endl;
+		}
+
 		void debug(std::string msg) {
-		
+			print(msg);
 		}
 
 		void info(std::string msg) {
-		
+			print(msg);
+		}
+
+		void error(std::string msg) {
+			print(msg);
 		}
 	}
 }
