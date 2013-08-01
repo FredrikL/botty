b04ca0bcb2b024f69f63a6a1468fe9c43fe6204d
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-08-01 12:44:47 +0200

    added engine class for message processing

---
+++ Botty/engine.cpp
@@ -0,0 +1,7 @@
+#include "engine.h"
+
+namespace botty {
+	std::string Engine::process_message(std::string msg) {
+		return nullptr;
+	}
+}; 
---
+++ Botty/engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace botty {
+	class Engine {
+	public:
+		std::string process_message(std::string);
+	};
+};
